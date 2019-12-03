a7c74b0f686463c195262e18c4c3be12ee72eea3
林嘉健 <1320579501@qq.com>
2019-12-03 11:10:04 +0800

    asdasd

--- c#/CommonCrawler/WindowsFormsApplication1/Program.cs
+++ c#/CommonCrawler/WindowsFormsApplication1/Program.cs
@@ -1,21 +1,10 @@
-﻿using Autofac;
-using Autofac.Extras.DynamicProxy;
-using log4net;
-using log4net.Config;
-using Snowflake.Net;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.IO;
-using System.Linq;
-using System.Net;
+﻿using System;
 using System.Runtime.InteropServices;
 
 namespace WindowsFormsApplication1
 {
     static class Program
     {
-        public static IContainer Container { private set; get; }
 
         [DllImport("kernel32.dll")]
         public static extern Boolean AllocConsole();
