7231e2c80a1f8c054452b31a6877ef5af084c39e
林嘉健 <1320579501@qq.com>
2019-12-04 10:09:02 +0800

    asdas

--- c#/CommonHelper/CommonHelper/Program.cs
+++ c#/CommonHelper/CommonHelper/Program.cs
@@ -24,19 +24,77 @@ namespace PA_Robot
         [STAThread]
         static void Main()
         {
-            bool ret = GuessHelper.GuessIDCard18Exist("************000000");
+            string ret = NumToChinese.NumConvertToChinese(1347859347);
             Console.WriteLine();
         }
-        static void test(string lockName)
-        {
-            lock (string.Intern(lockName))
-            {
-                for (var i =0;i<5;i++)
-                {
-                    Console.WriteLine($"当前线程正在占用：{lockName}，线程号：{Thread.CurrentThread.ManagedThreadId}");
-                    Thread.Sleep(1000);
-                }
-            }
-        }
+        /// <summary>
+        /// 数字转中文
+        /// </summary>
+        /// <param name="number">eg: 22</param>
+        /// <returns></returns>
+        //public static string NumberToChinese(this int number)
+        //{
+        //    string res = string.Empty;
+        //    string str = number.ToString();
+        //    string schar = str.Substring(0, 1);
+        //    switch (schar)
+        //    {
+        //        case "1":
+        //            res = "一";
+        //            break;
+        //        case "2":
+        //            res = "二";
+        //            break;
+        //        case "3":
+        //            res = "三";
+        //            break;
+        //        case "4":
+        //            res = "四";
+        //            break;
+        //        case "5":
+        //            res = "五";
+        //            break;
+        //        case "6":
+        //            res = "六";
+        //            break;
+        //        case "7":
+        //            res = "七";
+        //            break;
+        //        case "8":
+        //            res = "八";
+        //            break;
+        //        case "9":
+        //            res = "九";
+        //            break;
+        //        default:
+        //            res = "零";
+        //            break;
+        //    }
+        //    if (str.Length > 1)
+        //    {
+        //        switch (str.Length)
+        //        {
+        //            case 2:
+        //            case 6:
+        //                res += "十";
+        //                break;
+        //            case 3:
+        //            case 7:
+        //                res += "百";
+        //                break;
+        //            case 4:
+        //                res += "千";
+        //                break;
+        //            case 5:
+        //                res += "万";
+        //                break;
+        //            default:
+        //                res += "";
+        //                break;
+        //        }
+        //        res += NumberToChinese(int.Parse(str.Substring(1, str.Length - 1)));
+        //    }
+        //    return res;
+        //}
     }
 }
